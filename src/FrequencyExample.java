68204dc6f4393161af6539a8f8e559b0edfad907
Ompal Singh <ompal.singh_cs18@gla.ac.in>
2021-02-19 22:37:50 -0800

    frequency code

---
+++ src/FrequencyExample.java
@@ -0,0 +1,41 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Ompal Singh (Ompal-Singh)
+ *   Date: 2/19/2021
+ *   Time: 10:18 PM
+ *   File: FrequencyExample.java
+ */
+
+public class FrequencyExample {
+    public static void main(String[] args) {
+        //initialize array
+        int[] arr = new int[]{1, 2, 8, 3, 2, 2, 5, 1};
+        //Array will store frequencies of element
+        int[] fr = new int[arr.length];
+        int visited = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    //to avoid counting same element again
+                    fr[j] = visited;
+                }
+
+            }
+            if (fr[i] != visited)
+                fr[i] = count;
+
+        }
+        //Displays the frequency of each element present in array
+        System.out.println("-------------");
+        System.out.println("Element | Frequency");
+        System.out.println("--------------");
+        for (int i = 0; i < fr.length; i++) {
+            if (fr[i] != visited)
+                System.out.println("" + arr[i] + " | ");
+
+
+        }
+        System.out.println("----------------");
+    }
+}
